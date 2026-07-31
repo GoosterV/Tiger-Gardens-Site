d796a4a0e2129e1e15fa0e9ab65bd13a4a2ac79f
GoosterV <victorb94@gmail.com>
2026-07-30 23:59:14 -0700

    Add global D1Database fallback type for Vercel build

--- cloudflare-workers.d.ts
+++ cloudflare-workers.d.ts
@@ -1,5 +1,12 @@
-declare module "cloudflare:workers" {
-  export const env: {
-    DB?: unknown;
+declare global {
+  type Fetcher = {
+    fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
   };
+
+  type D1Database = any;
+  type D1Result = any;
+  type D1PreparedStatement = any;
+  type D1ResultData<T = unknown> = { results: T[] } | undefined;
 }
+
+export {};
