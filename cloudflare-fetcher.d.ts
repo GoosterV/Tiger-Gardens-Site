fe190fbea4324b19a14912c04aab6b5aff7f46ea
GoosterV <victorb94@gmail.com>
2026-07-31 00:08:15 -0700

    Update Cloudflare shim types for Vercel build

--- cloudflare-fetcher.d.ts
+++ cloudflare-fetcher.d.ts
@@ -1,3 +1,25 @@
 type Fetcher = {
   fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
 };
+
+type D1Database = any;
+type D1Result = any;
+type D1PreparedStatement = any;
+type D1ResultData<T = unknown> = { results: T[] } | undefined;
+
+declare module "cloudflare:workers" {
+  export interface Env {
+    ASSETS: Fetcher;
+    DB: D1Database;
+    IMAGES: {
+      input(stream: ReadableStream): {
+        transform(options: Record<string, unknown>): {
+          toBlob?: () => Promise<Blob>;
+          blob: () => Promise<Blob>;
+        };
+      };
+    };
+  }
+
+  export const env: Env;
+}
