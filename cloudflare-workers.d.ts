a72d19d2d3b9a1eb4740db0f49d0cb52862fbe09
GoosterV <victorb94@gmail.com>
2026-07-31 00:00:50 -0700

    Fix Vercel typecheck by adding cloudflare:workers shim

--- cloudflare-workers.d.ts
+++ cloudflare-workers.d.ts
@@ -1,11 +1,23 @@
-declare global {
-  type Fetcher = {
-    fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
+declare module "cloudflare:workers" {
+  export const env: {
+    ASSETS: Fetcher;
+    DB: D1Database;
+    IMAGES: {
+      input(stream: ReadableStream): {
+        transform(options: Record<string, unknown>): {
+          toBlob?: () => Promise<Blob>;
+          blob: () => Promise<Blob>;
+        };
+      };
+    };
   };
+}
 
+declare global {
+  type Fetcher = any;
   type D1Database = any;
-  type D1Result = any;
   type D1PreparedStatement = any;
+  type D1Result = any;
   type D1ResultData<T = unknown> = { results: T[] } | undefined;
 }
 
